32ad5f7edb5c2b0680dae5648eb89432f2e72afb
Nitish Singh <nitishsingh925@gmail.com>
2024-11-15 19:13:35 +0530

    shacdcn ui add with tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <h1>Hello Nitish </h1>
+
+      <Button>Click me</Button>
     </>
   );
 }
