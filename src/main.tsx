e4bdcca8313ea27821fe8f61e450d622066736bc
Nitish Singh <nitishsingh925@gmail.com>
2024-11-16 09:07:26 +0530

    TanStack Router v1 setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Import the generated route tree
+
+import { routeTree } from "./routeTree.gen.ts";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+
+// create a new router instance
+
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+//  Render the app
+
+const rootElement = document.getElementById("root")!;
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      {/* <App /> */}
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
+
+// createRoot(document.getElementById("root")!).render();
