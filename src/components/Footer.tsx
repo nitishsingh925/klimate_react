843d0300dd4b944f175cb2487b43f4d5bbdc2fe8
Nitish Singh <nitishsingh925@gmail.com>
2024-11-17 19:51:35 +0530

    add theme and chage footer colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="border-t backdrop-blur py-12">
+    <footer className="border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-auto px-4 text-center text-gray-400">
         <p>Made with ❤️ by Nitish Singh</p>
       </div>
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/context/theme-provider";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
@@ -5,13 +6,15 @@ import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="bg-gradient-to-br from-background to-muted">
-      <Header />
-      <main className="min-h-screen container mx-auto px-4 py-8">
-        <Outlet />
-      </main>
-      <Footer />
+    <ThemeProvider defaultTheme="system">
+      <div className="bg-gradient-to-br from-background to-muted">
+        <Header />
+        <main className="min-h-screen container mx-auto px-4 py-8">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
       <TanStackRouterDevtools />
-    </div>
+    </ThemeProvider>
   ),
 });
