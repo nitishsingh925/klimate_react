b366a4e38681b8608066485f861bf7ce18a3fc42
Nitish Singh <nitishsingh925@gmail.com>
2024-11-28 18:57:28 +0530

    add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,19 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{" "}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to="/">
+          <img src="/logo.png" alt="Klimate logo" className="h-12" />
+        </Link>{" "}
+        {/* <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link> */}
+      </div>
+      <div>
+        {/* search */}
+        {/* theme toggle */}
+      </div>
       <hr />
     </header>
   );
