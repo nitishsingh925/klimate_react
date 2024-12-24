f8b1485a94ecb5e9ca9c7b389e0ec7c6696ef5d8
Nitish Singh <nitishsingh925@gmail.com>
2024-12-24 22:35:00 +0530

    locationError alert set

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,15 +1,67 @@
+import WeatherSkeleton from "@/components/loading.skeletion";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { RefreshCw } from "lucide-react";
+import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
 function Index() {
-  const { coordinates, error, getLocation, isLoading } = useGeolocation();
+  const {
+    coordinates,
+    error: locationError,
+    getLocation,
+    isLoading: locationLoading,
+  } = useGeolocation();
+
   console.log(coordinates);
+
+  const handleRefresh = () => {
+    getLocation();
+    if (coordinates) {
+      //relode weather data
+    }
+  };
+
+  if (locationLoading) {
+    console.log(locationLoading);
+    return <WeatherSkeleton />;
+  }
+  if (locationError) {
+    console.log(locationError);
+
+    return (
+      <Alert variant={"destructive"}>
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4"></MapPin>
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!coordinates) {
+    return (
+      <Alert variant={"destructive"}>
+        <AlertTitle>Location Required </AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please enable location access to see your local weather.</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4"></MapPin>
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
 
   return (
     <div className="space-y-4">
@@ -18,7 +70,7 @@ function Index() {
         <Button
           variant={"outline"}
           size={"icon"}
-          // onClick={handleRefresh}
+          onClick={handleRefresh}
           // disabled={}
         >
           <RefreshCw className="h-4 w-4" />
